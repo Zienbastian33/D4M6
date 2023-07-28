81c4b3258cef16fb1faffc44beabb1806a2ba9bf
zienbastian33 <bastian.chacon93@gmail.com>
2023-07-28 06:32:42 -0400

    Primer commit, se configuran modelos, db y esquemas

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  def karina?
+    role == 'karina'
+  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
